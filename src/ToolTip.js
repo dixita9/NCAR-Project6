38307dfb3ad0bc1b7d56dff4b2432788599002a6
Anupriya Dixit <dixita@berea.edu>
2023-07-28 16:08:58 -0600

    Increased Tootip fint size

--- src/ToolTip.js
+++ src/ToolTip.js
@@ -1,20 +1,24 @@
-import React from 'react'; // Importing the React library
-import IconButton from '@mui/material/IconButton';// Importing the IconButton component from the Material-UI library
-import { Tooltip } from '@material-ui/core';  // Importing the Tooltip component from the Material-UI library
-import HelpIcon from '@mui/icons-material/Help';  // Importing the HelpIcon component from the Material-UI library
+import React from 'react';
+import IconButton from '@mui/material/IconButton';
+import { Tooltip } from '@mui/material';
+import HelpIcon from '@mui/icons-material/Help';
+import { makeStyles } from '@mui/styles';
 
-//Defining a functional component called CustomTooltip that accepts two props: title and placement (with a default value of 'top')
+const useStyles = makeStyles((theme) => ({
+  customTooltip: {
+    fontSize: '20px', // Change this value to your desired font size
+    color: 'white', // Additional custom styles for the tooltip
+  },
+}));
+
 export default function CustomTooltip({ title, placement = 'top' }) {
-  const tooltipStyle = {
-    color: "white",
-  };
-
+  const classes = useStyles();
 
   return (
     <div style={{ margin: '1px' }}>
-      <Tooltip title={title} placement={placement} style={tooltipStyle}>
-        <IconButton> 
-          <HelpIcon /> {/*Help Icon added*/}
+      <Tooltip title={title} placement={placement} classes={{ tooltip: classes.customTooltip }}>
+        <IconButton>
+          <HelpIcon />
         </IconButton>
       </Tooltip>
     </div>
